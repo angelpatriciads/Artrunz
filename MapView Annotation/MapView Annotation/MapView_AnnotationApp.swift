62aef2fb4d8c03f2f0f91d3d9da659ef05752256
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-05-21 20:50:16 +0700

    Finished Get User Location and Add MapView Annotation Project

---
+++ MapView Annotation/MapView Annotation/MapView_AnnotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapView_AnnotationApp.swift
+//  MapView Annotation
+//
+//  Created by Angelica Patricia on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MapView_AnnotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
