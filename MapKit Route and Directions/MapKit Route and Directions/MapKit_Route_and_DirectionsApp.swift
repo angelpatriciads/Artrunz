d755af8d24888d7bc1fe4e578f12437d14c09db5
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-05-21 18:38:32 +0700

    Add New App called MapKit, Route, and Directions

---
+++ MapKit Route and Directions/MapKit Route and Directions/MapKit_Route_and_DirectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_Route_and_DirectionsApp.swift
+//  MapKit Route and Directions
+//
+//  Created by Angelica Patricia on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_Route_and_DirectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
